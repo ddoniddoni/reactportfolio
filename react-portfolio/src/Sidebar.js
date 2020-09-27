f27ec383d90a5ea69c5ac5439d71bfc07c11e62e
ddoniddoni <psdkei@naver.com>
2020-09-27 18:45:51 +0900

    Delete Lotto

--- react-portfolio/src/Sidebar.js
+++ react-portfolio/src/Sidebar.js
@@ -43,9 +43,6 @@ const Sidebar = () => {
         <Link to="/wordrelay">
           <SidebarMenu title="WordRelay" route="wordrelay" />
         </Link>
-        <Link to="/lotto">
-          <SidebarMenu title="Lotto" route="lotto" />
-        </Link>
       </div>
     </div>
   );
